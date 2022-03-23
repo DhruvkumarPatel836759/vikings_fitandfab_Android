b8afc70542ae43b55dfe69d210cb259230370130
NaveenMannem <C0825762@mylambton.ca>
2022-03-23 17:24:13 -0400

    Edit profile

--- app/src/main/java/com/example/vikings_fitandfab_android/MainActivity.java
+++ app/src/main/java/com/example/vikings_fitandfab_android/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.vikings_fitandfab_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.vikings_fitandfab_android.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-//To check..
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+            }
+        });
+    }
 }
