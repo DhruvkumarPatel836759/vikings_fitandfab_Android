f544c714f6da7f1efb1f461e20761f59be76c32d
patel mayur <patelmayur3479@gmail.com>
2022-03-27 10:12:43 -0400

    created required supplement models and added design for shopping list

---
+++ app/src/main/java/com/example/vikings_fitandfab_android/Class/CartModel.java
@@ -0,0 +1,44 @@
+package com.example.vikings_fitandfab_android.Class;
+
+public class CartModel {
+
+    String productId,cId;
+    boolean selection =true;
+
+    public CartModel() {
+    }
+
+    public boolean isSelection() {
+        return selection;
+    }
+
+    public void setSelection(boolean selection) {
+        this.selection = selection;
+    }
+
+    SupplimentModel supplimentModel;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public SupplimentModel getSupplimentModel() {
+        return supplimentModel;
+    }
+
+    public void setSupplimentModel(SupplimentModel supplimentModel) {
+        this.supplimentModel = supplimentModel;
+    }
+}
---
+++ app/src/main/java/com/example/vikings_fitandfab_android/Class/SupplimentModel.java
@@ -0,0 +1,68 @@
+package com.example.vikings_fitandfab_android.Class;
+
+public class SupplimentModel {
+
+    String discription,image,name,type,sId;
+    int quantity;
+    double price;
+
+    public SupplimentModel() {
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
