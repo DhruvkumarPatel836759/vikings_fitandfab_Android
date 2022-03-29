950b03b3add8c809ad9081fd0c9bbd5f30f9e997
patel mayur <patelmayur3479@gmail.com>
2022-03-29 19:11:49 -0400

    Added Ordermodel and java class

---
+++ app/src/main/java/com/example/vikings_fitandfab_android/Class/OrderModel.java
@@ -0,0 +1,56 @@
+package com.example.vikings_fitandfab_android.Class;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OrderModel {
+    String orderId,userId,status;
+    ArrayList<HashMap<String, Object>> products = new ArrayList<>();
+
+    double payment;
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    public OrderModel() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public ArrayList<HashMap<String, Object>> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<HashMap<String, Object>> products) {
+        this.products = products;
+    }
+}
