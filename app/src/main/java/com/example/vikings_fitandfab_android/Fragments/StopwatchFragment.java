c4486cb78eddc259b977ff5554bbc77d37b72368
manjit <manjit>
2022-03-29 18:25:49 -0400

    Edit StopWatch

--- app/src/main/java/com/example/vikings_fitandfab_android/Fragments/StopwatchFragment.java
+++ app/src/main/java/com/example/vikings_fitandfab_android/Fragments/StopwatchFragment.java
@@ -1,66 +1,64 @@
 package com.example.vikings_fitandfab_android.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.vikings_fitandfab_android.R;
+import com.yashovardhan99.timeit.Stopwatch;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StopwatchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class StopwatchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StopwatchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StopwatchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StopwatchFragment newInstance(String param1, String param2) {
-        StopwatchFragment fragment = new StopwatchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView stopwatchText;
+    Button startButton;
+    boolean start=false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        stopwatchText=view.findViewById(R.id.stopwatchText);
+        startButton=view.findViewById(R.id.startButton);
+
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.setTextView(stopwatchText);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!start){
+                    startButton.setText("Stop");
+                    stopwatch.start();
+                    start=true;
+                }else{
+                    startButton.setText("Start");
+                    stopwatch.stop();
+                    start=false;
+                }
+
+
+            }
+        });
+
+
+
+
+
     }
 }
