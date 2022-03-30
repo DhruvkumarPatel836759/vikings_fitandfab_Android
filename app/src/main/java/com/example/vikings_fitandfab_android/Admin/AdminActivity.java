033b1650d294e7bb6b1f044bfc9fdca595aecf94
patel mayur <patelmayur3479@gmail.com>
2022-03-30 17:14:34 -0400

    Added design of product details

--- app/src/main/java/com/example/vikings_fitandfab_android/Admin/AdminActivity.java
+++ app/src/main/java/com/example/vikings_fitandfab_android/Admin/AdminActivity.java
@@ -3,12 +3,16 @@ package com.example.vikings_fitandfab_android.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.vikings_fitandfab_android.LoginActivity;
+import com.example.vikings_fitandfab_android.R;
 import com.example.vikings_fitandfab_android.databinding.ActivityAdminBinding;
 
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity  extends AppCompatActivity {
 
 
     ActivityAdminBinding binding;
@@ -18,5 +22,30 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityAdminBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSharedPreferences("Gym_ref", MODE_PRIVATE).edit()
+                        .putString("admin", "")
+                        .commit();
+                startActivity(new Intent(AdminActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+        binding.AddProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminActivity.this,ProductActivity.class));
+            }
+        });
+//
+        binding.orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminActivity.this, OrderAdminActivity.class));
+            }
+        });
     }
 }
